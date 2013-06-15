89e148da06f7ef57acc3529c60018e61976407b7
wxqqh <wxqqhh@gmail.com>
2013-06-16 01:44:18 +0800

    完善modulejs
    补充完成require和defined方法

--- modulejs/src/module.js
+++ modulejs/src/module.js
@@ -1,8 +1,17 @@
-;~function () {
+;(function (global) {
 	var ID = 0,
+		def = ["require", "exports", "module"],
 		modules = {},
 		is = function(type, obj){
 			return Object.prototype.toString.call(obj).slice(8, -1) === type;
+		},
+		swap = function(a, o){
+			Object.keys(o).forEach(function (k) {
+				var v = o[k],
+					i = a.indexOf(k);
+				(i > -1) && (a[i] = v);
+			})
+			return a;
 		},
 		module = function(id, deps, factory) {
 			this.id = id;
@@ -10,18 +19,57 @@
 			this.factory = factory;
 			this.exports = null;
 		},
+		getExports = function(id){
+			var factory, deps, exports, defDep, depList = {};
+			m = modules[id]
+			if(!m) return null;
+			if(m.exports) return m.exports;
+
+			factory = m.factory,
+			deps = m.deps.concat(def),
+			defDep ={
+				'require': require,
+				'module': m,
+				'exports': (m.exports = {})
+			};
+
+			if(is('Function', factory)){
+				deps.forEach(function (dep) {
+					depList[dep] = def.indexOf(dep) > -1 ? defDep[dep] : getExports(dep);
+				});
+				exports = factory.apply(m, factory.length ? swap(deps, depList) : []);
+			} else {
+				exports = factory;
+			}
+
+			m.exports = exports || m.exports;
+
+			return m.exports;
+		},
 		defined = function(id, deps, factory) { // 模块定义必须要有id
 			var m;
-			is('function', deps) && (factory=deps, deps= null); // 如果deps是一个函数，则覆盖factory，deps置空
+			is('Function', deps) && (factory=deps, deps= null); // 如果deps是一个函数，则覆盖factory，deps置空
 			deps = deps ? [].concat(deps) : [];
 			m = new module(id, deps, factory);
 			modules[id] = m;
+			return id;
 		},
-		require = function(ids,fn) {
+		require = function(ids, fn) {
+			var fnArgs, reqs = {}, 
+			ids = [].concat(ids);
 
-		},
+			ids.forEach(function(id){
+			 	reqs[id] = def.indexOf(id) > -1 ? defDep[id] : getExports(id);
+			});
+
+			is('Function', fn) && fn.apply(null, ids.length ? swap(ids , reqs) : []);
+
+			return ids.length == 1 ? reqs[ids[0]] : reqs;
+		};
 		require.async = function(ids, fn){
-
-		}
-
-}
+			setTimeout(function() { require(ids, fn) }, 0);
+		};
+	global.modules =modules,
+	global.require = require,
+	global.defined = defined;
+})(window);
