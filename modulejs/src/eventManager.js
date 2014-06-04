2aab8ead1a430a66e3a62a2bd581df9199293672
wxqqh <wxqqhh@gmail.com>
2014-06-04 21:45:17 +0800

    修改一下log的位置

--- modulejs/src/eventManager.js
+++ modulejs/src/eventManager.js
@@ -8,6 +8,7 @@ define("EventManager", function() {
 			this.env.on.apply(this.env, arguments);
 		},
 		emit: function(type /* args */) {
+			console.group("EventManager#emit type : %s ", type); // @debug
 			var interceptor;
 			if(type.indexOf("@") > -1) { // 如果是触发扩展事件
 				var temp = type.split("@");
@@ -15,16 +16,17 @@ define("EventManager", function() {
 				interceptor = temp[1];
 				
 				if(this.invocations[interceptor]) {
+					console.time("EventManager#emit invoke : %s ", interceptor); // @debug
 					this.invocations[interceptor].apply(this, arguments);
+					console.timeEnd("EventManager#emit invoke : %s ", interceptor); // @debug
 				} else {
 					console.warn("EventManager#emit interceptor : %s had not defined!", interceptor); // @debug
 				}
 
 			} else { // 直接触发事件
-				console.group();
+			
 				for(interceptor in this.invocations) { // 触发所有类型的interceptor
 					var invocation = this.invocations[interceptor];
-					console.info("EventManager#emit invoke : %s ", interceptor); // @debug
 					console.time("EventManager#emit invoke : %s ", interceptor); // @debug
 					invocation.apply(this, arguments);
 					console.timeEnd("EventManager#emit invoke : %s ", interceptor); // @debug
@@ -32,8 +34,8 @@ define("EventManager", function() {
 				console.time("EventManager#emit emit : %s ", type); // @debug
 				this.env.emit.apply(this.env, arguments);
 				console.time("EventManager#emit emit : %s ", type); // @debug
-				console.groupEnd();
 			}
+			console.groupEnd(); // @debug
 		},
 		add: function(interceptor, invocation) {
 			if(!this[interceptor]) {
