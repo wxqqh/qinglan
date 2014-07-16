8970d9ad98c72669b442a81cc96eac60e0ad8e21
wxqqh <wxqqhh@gmail.com>
2014-07-16 14:24:51 +0800

    精简module.js
    把一些不必要的逻辑去掉, 为之后的扩展做准备

--- modulejs/src/module.js
+++ modulejs/src/module.js
@@ -1,116 +1,65 @@
 ;(function (global) {
 	var modules = {},
-		def = ["require", "exports", "module"],
-		is = function(type, obj){
-			return Object.prototype.toString.call(obj).slice(8, -1) === type;
-		},
-		swap = function(a, o){
-			Object.keys(o).forEach(function (k) {
-				var v = o[k],
-					i = a.indexOf(k);
-				(i > -1) && (a[i] = v);
-			})
-			return a;
-		},
-		module = function(id, deps, factory) {
+		Module = function(id, deps, factory) {
 			this.id = id;
 			this.deps = deps || [];
 			this.factory = factory;
 			this.exports = null;
-			this.loader= null;
-			this.plugins = [];
 		},
 		getExports = function(id){
-			var factory, deps, exports, defDep, depList = {}, m = modules[id], duration = 0;
+			var m = modules[id]; 
+			
+			if(!m) {return null;}
+			if(m.exports) {return m.exports;}
 
-			if(id.indexOf('!') > -1) { // 加载同步插件
-				return getPlugin(id);
-			}
+			var duration = Date.now(); // @debug
 
-			if(!m) return null;
-			if(m.exports) return m.exports;
+			if("function" == typeof m.factory) { // 如果factory是一个Function, 则需要获取依赖然后执行
+				var reqs = [];
+				
+				m.exports = {};
 
-			factory = m.factory,
-			deps = m.deps.concat(def),
-			defDep ={
-				'require': require,
-				'module': m,
-				'exports': (m.exports = {})
-			};
-
-			if(is('Function', factory)){
-				deps.forEach(function (dep) {
-					depList[dep] = def.indexOf(dep) > -1 ? defDep[dep] : getExports(dep);
+				m.deps.forEach(function (dep) {
+					reqs.push(getExports(dep));
 				});
-				duration = Date.now(); // @debug
-				exports = factory.apply(m, factory.length ? swap(deps, depList) : []);
-				duration = Date.now() - duration; // @debug
-			} else {
-				exports = factory;
+
+				reqs = reqs.concat(require/* require */, (m.exports = {})/* exports */, m /* module */);
+			
+				var exports = m.factory.apply(m, reqs);
+				
+				m.exports = exports || m.exports;
+
+			} else { // 是一个值
+				m.exports = m.factory;
 			}
 
-			m.exports = exports || m.exports;
+			duration = Date.now() - duration; // @debug
 			console.log('module getExports id : ' + id + ' duration : ' + duration); // @debug
+			
 			return m.exports;
 		},
-		getPlugin = function(id) {
-			var parts = id.split('!'), pluginId = parts[0] || '', plugSource = parts[1] || '', pluginLoader, plugin = modules[id], duration = 0, load, definition;
-			
-			if(plugin && plugin.exports) { 
-				return plugin.exports; //如果这个资源ID已经被初始化过，则直接返回
-			} else if(plugSource) {
-				plugin = new module(id); // 初始化资源模块（被加载插件）
-				duration = Date.now(); // @debug
-				pluginLoader = getExports(pluginId); // 获取插件加载器
-				if(pluginLoader) {
-					load = function (definition) { // 插件加载完毕
-						plugin.exports = definition || {};
-						modules[id] = plugin; // 覆盖pluginModule
-					};
-					pluginLoader.normalize && (plugSource = pluginLoader.normalize(plugSource)); // 如果加载器定义了normalize方法，则调用它，得到标准化之后的plugSource
-					pluginLoader.load &&  (definition = pluginLoader.load(plugSource, require, load, define.pluginLoader)); // 执行加载器定义的load方法，所有插件加载模块都必须实现的load方法
-					!plugin.exports && load(definition); // 如果module的exports没有被赋值，则把load方法的返回值覆盖
-					duration = Date.now() - duration; // @debug
-					console.log('module getPlugin id : ' + id + ' duration : ' + duration); // @debug
-
-					plugin.loader = pluginLoader;
-					modules[pluginId].plugins.push(plugin.exports);
-					return plugin.exports; // 返回插件模块加载好的模块
-				} else {
-					return null;
-				}
-			}
-		},
 		define = function(id, deps, factory) { // 模块定义必须要有id
-			var m;
 			factory == void 0 && (factory=deps, deps= null); // 如果factory是undefined，则覆盖factory，deps置空
 			deps = deps ? [].concat(deps) : [];
-			m = new module(id, deps, factory);
-			modules[id] = m;
+			modules[id] = new Module(id, deps, factory);
+			
 			console.log('module define id : ' + id); // @debug
+			
 			return id;
 		},
-		require = function(ids, fn) {
-			var fnArgs, reqs = {},
-			ids = [].concat(ids),
-			len = ids.length;
+		require = function(ids) {
+			var reqs = [];
+
+			ids = [].concat(ids);
 
 			ids.forEach(function(id){
-			 	reqs[id] = getExports(id);
+			 	reqs.push(getExports(id));
 			});
 
-			is('Function', fn) && fn.apply(null, len ? swap(ids , reqs) : []);
-
-			return len == 1 ? reqs[ids[0]] : reqs;
+			return reqs.length == 1 ? reqs[0] : reqs;
 		};
 
-	define.plugin = {};
-
-	require.async = function(ids, fn){
-			setTimeout(function() { require(ids, fn) }, 0);
-		};
-
-	global.modules = modules,
-	global.require = require,
+	global.modules = modules;
+	global.require = require;
 	global.define = define;
 })(window);
